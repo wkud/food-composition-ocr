433a9d3cf91b990eced971170f533f51df08d789
mgurski <mateuszgurski444@gmail.com>
2021-03-16 17:30:52 +0100

    Description field and admin panel edit for Eadditives model

--- server/django_app/eadditives/models.py
+++ server/django_app/eadditives/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Eadditives(models.Model):
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=300)
-    
+    description = models.TextField(default=None, blank=True, null=True)
+
     def __str__(self):
-        return "E" + self.code, self.name
+        return '{} {}'.format("E" + self.code, self.name)
