8df4e9595e476961df4c6a33341011d09d51619b
Wojtek <vopox07@gmail.com>
2021-03-20 22:35:39 +0100

    Replace commas with semicolons in interface declaration

--- client/react_app/src/components/GenericButton.tsx
+++ client/react_app/src/components/GenericButton.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 interface Props {
-  onClick: React.MouseEventHandler<HTMLElement>,
-  iconClass: string,
-  text: string,
+  onClick: React.MouseEventHandler<HTMLElement>;
+  iconClass: string;
+  text: string;
 }
 
-const GenericButton = ({onClick, iconClass, text}: Props) => {
+const GenericButton = ({ onClick, iconClass, text }: Props) => {
   return (
     <Button variant='primary' className='button-size p-2' onClick={onClick}>
       <i className={iconClass} /> {text}
