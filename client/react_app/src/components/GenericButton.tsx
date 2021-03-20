03f85f9ddc4b52a8c533696027ec48653e381e66
Wojtek <vopox07@gmail.com>
2021-03-20 22:28:52 +0100

    Make buttons bigger. Fix prop's type

--- client/react_app/src/components/GenericButton.tsx
+++ client/react_app/src/components/GenericButton.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {Button} from 'react-bootstrap';
 
 interface Props {
-  onClick: React.MouseEventHandler<HTMLElement> | undefined,
+  onClick: React.MouseEventHandler<HTMLElement>,
   iconClass: string,
   text: string,
 }
 
 const GenericButton = ({onClick, iconClass, text}: Props) => {
   return (
-    <Button variant='primary' className='button-size' onClick={onClick}>
+    <Button variant='primary' className='button-size p-2' onClick={onClick}>
       <i className={iconClass} /> {text}
     </Button>
   );
