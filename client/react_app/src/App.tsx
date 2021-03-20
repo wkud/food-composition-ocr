9acb6d44e554bdde6cc318e72c79bd5d0f10434d
Wojtek <vopox07@gmail.com>
2021-03-20 18:58:10 +0100

    Create page layout and styling

--- client/react_app/src/App.tsx
+++ client/react_app/src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'react-bootstrap';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page-size d-flex align-items-center justify-content-center'>
+      <div className='body-size w-75 h-75 d-flex flex-column justify-content-between text-center align-items-center'>
+        <div>
+          <h3>Product Composition Recognizer</h3>
+          <div className='lead d-none d-sm-block'>
+            Product composition recognizer allows you to easily analyze
+            composition of food products. Simply take a photo of product's
+            composition list and application will show you the name and
+            description of every E-additive present in the food product.
+          </div>
+          <div className='lead d-sm-none'>
+            Take a photo of product's composition and the list of E-additives
+            will be displayed soon.
+          </div>
+        </div>
+        <Button variant='primary' className='button-size'>
+          Upload
+        </Button>
+        <Button variant='primary' className='button-size'>
+          Take a photo
+        </Button>
+        <div />
+      </div>
     </div>
   );
 }
